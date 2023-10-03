eb2a1893d8ec1d6cf49c239f39ad88ff4f8ed871
Raymundo Morales <mamusho@gmail.com>
2023-10-03 14:18:54 -0600

    commit from intelliJ

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findByActivoTrue(Pageable paginacion);
 
+// primer cambio IntelliJ
 
     @Query("""
             select m from Medico m
